4909f861589eedc4d45100505a3b488e3355314b
kenmaro3 <miha.ken.19@gmail.com>
2022-06-27 06:34:21 +0000

    [UPD] measurement with tflite model

--- unet2d-128/eval_tf_lite.py
+++ unet2d-128/eval_tf_lite.py
@@ -1,22 +1,12 @@
+import tensorflow as tf
 import cv2 as cv
 import numpy as np
-import tensorflow as tf
-from unet2d import UNet2D
-import pandas as pd
-import matplotlib.pyplot as plt
-import time
-import dl_eval
-from PIL import Image
 import os
+import time
 import shutil
-
-physical_devices = tf.config.list_physical_devices('GPU')
-try:
-    tf.config.experimental.set_memory_growth(physical_devices[0], True)
-except:
-    pass
-
-
+from PIL import Image
+import dl_eval
+import matplotlib.pyplot as plt
 
 def calculate_stat_helper(predict, label):
     test1 = predict
@@ -34,8 +24,18 @@ def calculate_stat_helper(predict, label):
     return precision, recall, fvalue, iou
 
 
-
 if __name__ == "__main__":
+    print("hello, world")
+    tf_lite_path = "model_128.tflite"
+
+    interpreter = tf.lite.Interpreter(tf_lite_path)
+
+    interpreter.allocate_tensors()
+
+    input_details = interpreter.get_input_details()[0] 
+    #print(input_details)
+    output_details = interpreter.get_output_details()[0]
+    #print(output_details)
 
     test_path = "/home/ubuntu/workdir/nur/semantic-segmentation/data/CAR/validation/video/eval0.mp4"
     label = "/home/ubuntu/workdir/nur/semantic-segmentation/data/CAR/validation/video_label/eval0_bin.mp4"
@@ -50,23 +50,6 @@ if __name__ == "__main__":
 
     ## Generate model
     inputs  = tf.keras.layers.Input(shape=(128, 128, 3))
-    model   = UNet2D(n_channels = 3, n_classes = 1)        ##Note: Do we need to simplify this too?
-    outputs = model(inputs)
-    model   = tf.keras.Model(inputs, outputs)
-
-    model.load_weights(f"model_128.h5")
-
-    # converter = tf.lite.TFLiteConverter.from_keras_model(model)
-
-    # converter.optimizations = [tf.lite.Optimize.DEFAULT]
-    # converter.target_spec.supported_types = [tf.float16]
-
-    # tflite_model = converter.convert()
-
-    # tflite_model_file = "model_128.tflite"
-    # tflite_model_file.write_bytes(tflite_model)
-
-    # quit()
 
 
     res_folder = "./result"
@@ -77,7 +60,7 @@ if __name__ == "__main__":
         os.mkdir(res_folder)
 
     dl = dl_eval.get_dl(batch_size=1)
-    loop = 50
+    loop = 5
     time_list = []
     for i in range(loop):
         print(f"{i} th loop =======================")
@@ -86,9 +69,15 @@ if __name__ == "__main__":
         frames = frames.astype("float32")
         labels = np.where(labels < 0.5, 0, 255)
         labels = labels.astype(np.uint8)
+        frames = frames.astype(np.float32)
 
         t1 = time.time()
-        res = model.predict(frames)
+
+        interpreter.set_tensor(input_details['index'], frames)
+
+        interpreter.invoke()
+        res = interpreter.get_tensor(output_details['index'])
+        #res = model.predict(frames)
         t2 = time.time()
         time_list.append(t2-t1)
 
@@ -101,18 +90,18 @@ if __name__ == "__main__":
         frames = frames*255.0
         frames = frames.astype(np.uint8)
         pil_image=Image.fromarray(frames[0])
-        # pil_image.save(f"{res_folder}/pil_{i}.png")
-        # plt.imsave(f'{res_folder}/res_{i}.png', tmp[:, :, 0], cmap='gray')
-        # plt.imsave(f'{res_folder}/label_{i}.png', labels[0], cmap='gray')
+        pil_image.save(f"{res_folder}/pil_{i}.png")
+        plt.imsave(f'{res_folder}/res_{i}.png', tmp[:, :, 0], cmap='gray')
+        plt.imsave(f'{res_folder}/label_{i}.png', labels[0], cmap='gray')
 
-        # res_cv = cv.cvtColor(tmp, cv.COLOR_GRAY2BGR)
-        # frame = cv.cvtColor(frames[0], cv.COLOR_RGB2BGR)
-        # img_merged = cv.addWeighted(frame, 1.0, res_cv, 1.0, 0.1)
-        # img_merged_resize = cv.resize(img_merged, dsize=(640, 480))
-        # cv.imwrite(f"{res_folder}/merged_{i}.png", img_merged_resize)
+        res_cv = cv.cvtColor(tmp, cv.COLOR_GRAY2BGR)
+        frame = cv.cvtColor(frames[0], cv.COLOR_RGB2BGR)
+        img_merged = cv.addWeighted(frame, 1.0, res_cv, 1.0, 0.1)
+        img_merged_resize = cv.resize(img_merged, dsize=(640, 480))
+        cv.imwrite(f"{res_folder}/merged_{i}.png", img_merged_resize)
 
-        # stat = calculate_stat_helper(tmp, labels[0])
-        # print(f"stat: {stat}")
+        stat = calculate_stat_helper(tmp, labels[0])
+        print(f"stat: {stat}")
 
     print("time average:")
     print(time_list)
