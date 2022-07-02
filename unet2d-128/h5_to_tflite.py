73ed3ba98555eb098867e06c5417121280ddf771
kenmaro3 <miha.ken.19@gmail.com>
2022-07-02 05:26:50 +0000

    [UPD] fixed batch size to "fix" graph

--- unet2d-128/h5_to_tflite.py
+++ unet2d-128/h5_to_tflite.py
@@ -6,7 +6,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
-    inputs  = tf.keras.layers.Input(shape=(128, 128, 3))
+    inputs  = tf.keras.layers.Input(shape=(128, 128, 3), batch_size=1)
     model   = UNet2D(n_channels = 3, n_classes = 1)        ##Note: Do we need to simplify this too?
     outputs = model(inputs)
     model  = tf.keras.Model(inputs, outputs)
@@ -14,17 +14,19 @@ if __name__ == "__main__":
     model.load_weights(f"model_128.h5")
 
     input_model = "saved_model"
-    output_model = "model_128_dynamic.tflite"
+    output_model = "model_128_dr_1.tflite"
 
     print("\n\nhere0000!!!=================")
     converter = tf.lite.TFLiteConverter.from_keras_model(model)
     converter.optimizations = [tf.lite.Optimize.DEFAULT]
-    #converter.target_spec.supported_types = [tf.float16]
+    # converter.target_spec.supported_types = [tf.float16]
     tflite_model = converter.convert()
     print("\n\nhere!!!=================")
+    #with open(output_model, 'wb') as o_:
+    #    o_.write(tflite_model)
+
     with open(output_model, 'wb') as o_:
         o_.write(tflite_model)
-
 
 
     # model.save(input_model)
